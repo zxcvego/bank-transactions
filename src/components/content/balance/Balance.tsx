15212f962a6da26cd33e8d686e82f8833ab60f73
zxcvego <zxcvego@gmail.com>
2022-11-20 14:28:29 +0100

    feat: balance design

--- src/components/content/balance/Balance.tsx
+++ src/components/content/balance/Balance.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./Balance.module.css";
 
 const Balance = () => {
-	return <div>Balance</div>;
+	return (
+		<section>
+			<div className={styles.balance}>
+				<h1>Your balance:</h1>
+				<p>999 PLN</p>
+			</div>
+		</section>
+	);
 };
 
 export default Balance;
