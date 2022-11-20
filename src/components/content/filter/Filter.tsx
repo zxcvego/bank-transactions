aea5cfff4b22433000eaf0423227c27d81757e7e
zxcvego <zxcvego@gmail.com>
2022-11-20 14:34:46 +0100

    feat: filter design

--- src/components/content/filter/Filter.tsx
+++ src/components/content/filter/Filter.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./Filter.module.css";
 
 const Filter = () => {
-	return <div>Filter</div>;
+	return (
+		<section>
+			<div className={styles.filter}>
+				<h1>Search by name</h1>
+				<input placeholder="e.g. John Doe" maxLength={20} />
+			</div>
+		</section>
+	);
 };
 
 export default Filter;
